6ba350be536aa8761499412422c50a497f714a9a
nriggi15 <77181170+nriggi15@users.noreply.github.com>
2025-05-16 17:02:14 -0400

    Blog deploy test
    Blog deploy test

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 export default defineConfig({
   integrations: [mdx()],
-  base: '/blog',  // 🔥 This makes links relative under /blog
+   base: '/blog',  // ✅ needed for correct subfolder deploy under mycardverse.com/blog
 });
