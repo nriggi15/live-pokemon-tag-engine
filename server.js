21c8dc0e76da0e196aa6d4497ab1c35fa8c08111
nriggi15 <77181170+nriggi15@users.noreply.github.com>
2025-05-18 23:34:37 -0400

    update proxy
    update proxy

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config(); // Always first
 
-app.set('trust proxy', 1);
-
 import express from 'express';
 import session from 'express-session';
 import mongoose from 'mongoose';
@@ -22,6 +20,8 @@ import favoritesRouter from './routes/favorites.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('trust proxy', 1);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
